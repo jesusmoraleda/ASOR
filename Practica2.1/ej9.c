a409f8e2382af6677f39ec73460893ad14c2269d
Jesus Martin Moraleda <jesusmartinmoraleda@gmail.com>
2020-12-06 11:09:44 +0100

    Información del usuario

---
+++ Practica2.1/ej9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Gestión de errores:
+#include <errno.h>
+//Getuid/sysconf
+#include <unistd.h>
+//Perror
+#include <string.h>
+//Uname
+#include <sys/utsname.h>
+//id
+#include <sys/types.h>
+//passwd
+#include <pwd.h>
+
+int main() {
+
+	uid_t uid = getuid();
+
+	printf("UID Real: %d\n", getuid());
+	printf("UID efectivo: %d\n", geteuid());
+
+	return 1;
+}
